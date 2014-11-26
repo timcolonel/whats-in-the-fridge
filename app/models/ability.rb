7189d1be03f1fd7be2acf5ca3d17b64a948d8c8a
Timothee Guerin <timothee.guerin@outlook.com>
2014-11-26 15:30:41 -0500

    added adaptater

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,12 +2,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    can [:show, :index, :list], Ingredient
+    can [:show, :index, :list], Recipe
+
     return if user.nil?
     if user.admin?
       can :manage, :all
     else
-      can [:show, :index, :list], Ingredient
-      can [:show, :index, :list], Recipe
+
       can [:create, :update, :destroy], Recipe, user_id: user.id
     end
   end
